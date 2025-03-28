e44a3e392ee01ffcbca9c539e1b33e7b1de0af4f
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2025-03-29 00:14:22 +0700

    view absensi

--- resources/views/absensi/absensi.blade.php
+++ resources/views/absensi/absensi.blade.php
@@ -1,5 +1,34 @@
 <x-guest-layout>
-    halo
+    <h1 class="text-center font-bold text-xl">Pilih Keanggotaanmu</h1>
+    <div class="mt-10">
+        <a
+            href="{{route('register')}}"
+            class="border-4 border-orange-400 rounded-lg px-4 py-2 font-semibold hover:bg-orange-400 hover:text-white transition delay-75 mr-8"
+        >
+            Anggota Aktif
+        </a>
+
+        <a
+            href="{{route('register')}}"
+            class="border-4 border-orange-400 rounded-lg px-4 py-2 font-semibold hover:bg-orange-400 hover:text-white transition delay-75 mr-8"
+        >
+            Anggota Muda
+        </a>
+
+        <a
+            href="{{route('register')}}"
+            class="border-4 border-orange-400 rounded-lg px-4 py-2 font-semibold hover:bg-orange-400 hover:text-white transition delay-75 mr-8"
+        >
+            Anggota Luar Biasa
+        </a>
+
+        <a
+            href="{{route('register')}}"
+            class="border-4 border-orange-400 rounded-lg px-4 py-2 font-semibold hover:bg-orange-400 hover:text-white transition delay-75 mr-8"
+        >
+            Lembaga Lain
+        </a>
+    </div>
 </x-guest-layout>
 
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-6 px-12 py-16 bg-white shadow-2xl overflow-hidden sm:rounded-3xl">
+            <div class="mt-6 px-12 py-16 bg-white shadow-2xl overflow-hidden sm:rounded-3xl">
                 {{ $slot }}
             </div>
         </div>
