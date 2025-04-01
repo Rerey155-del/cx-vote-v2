79d97e0e69423de2075acc8ba07e8cb2b6ce0bd1
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2025-04-01 23:39:42 +0700

    UI Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,24 @@
 <x-admin-layout title="Dashboard">
+    <div class="px-12 grid grid-cols-3 gap-4">
+        <div class="bg-orange-400 rounded-xl flex justify-center items-center py-12">
+            <div class="text-center text-white font-semibold text-xl">
+            <h2 class="mb-4">Already Voted</h2>
+            <span>10</span>
+            </div>
+        </div>
 
+        <div class="bg-red-400 rounded-xl flex justify-center items-center py-12">
+            <div class="text-center text-white font-semibold text-xl">
+            <h2 class="mb-4">Not Yet Voted</h2>
+            <span>10</span>
+            </div>
+        </div>
+
+        <div class="bg-purple-400 rounded-xl flex justify-center items-center py-12">
+            <div class="text-center text-white font-semibold text-xl">
+            <h2 class="mb-4">Attendance</h2>
+            <span>10</span>
+            </div>
+        </div>
+    </div>
 </x-admin-layout>
