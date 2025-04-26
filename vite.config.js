d31a49bf4a06781745995a59c275d90dee6d4bf7
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2025-04-26 18:46:16 +0700

    Local Server

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server:{
+        // Atur sesuai ip yang dapat di lihat id cmd ipconfig
+        host:"",
+        port: 8000
+    }
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
